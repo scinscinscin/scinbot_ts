47d027ce20ca28b906cf9215a35bf9b5a22a306b
scinorandex <scinorandex@gmail.com>
2021-02-10 07:55:30 +0800

    add test command

---
+++ commands/test.ts
@@ -0,0 +1,20 @@
+export {};
+//const fs = require('fs');
+//const commit = fs.readFileSync(`gitCommit`, {encoding:'utf8', flag:'r'});
+const { colors } = require('../config/main.json');
+const green = colors.green;
+
+function test(args: string[], authorID: string, author: string, channelID: string, channel: any, creator: any, bot: any){
+    let message: string;
+    message =  "**";
+    message += "Info: \n";
+    //message += `Commit Hash: ${commit}`;
+    message += `Author: ${author} \n`;
+    message += `Author ID: ${authorID} \n`;
+    message += `Channel ID: ${channelID} \n`;
+    message += `Arguments: ${args.join(" ")} \n`;
+    message +=" **";
+    return({"color": green, "title": "Test Successful", "message": message});
+}
+
+module.exports = test;
