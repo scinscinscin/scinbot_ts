974df2b67c64588dcd6bff1c0c0aee37ccc64cce
scinorandex <scinorandex@gmail.com>
2021-02-11 17:52:15 +0800

    refactor ping command

--- commands/ping.ts
+++ commands/ping.ts
@@ -5,9 +5,9 @@ const { green } = colors;
 function ping(args: string[], authorID: string, author: string, channelID: string, channel: any, creator: any, bot: any, messageObject: any){
     let messageSent: number = messageObject.createdTimestamp;
     let currentTime: number = Math.floor(Date.now());
-    let ping: number = (messageSent - currentTime) * -1;
+    let ping: number = currentTime - messageSent;
     
-    return({"color": green, "title": `Ping`, "message": `${ping}ms`});
+    return({"color": green, "title": `Ping`, "message": `**${ping}ms**`});
 }
 
 module.exports = ping;
