0b50f7ac31646ed33144da7be5bace8e263e0e06
scinorandex <scinorandex@gmail.com>
2021-02-11 17:48:06 +0800

    add ping command

---
+++ commands/ping.ts
@@ -0,0 +1,14 @@
+export{};
+const { colors } = require('../config/main.json');
+const { green } = colors;
+
+function ping(args: string[], authorID: string, author: string, channelID: string, channel: any, creator: any, bot: any, messageObject: any){
+    let messageSent: number = messageObject.createdTimestamp;
+    let currentTime: number = Math.floor(Date.now());
+    let ping: number = (messageSent - currentTime) * -1;
+    
+    return({"color": green, "title": `Ping`, "message": `${ping}ms`});
+}
+
+module.exports = ping;
+module.exports.help = "Ping scinbot_ts and get response time";
