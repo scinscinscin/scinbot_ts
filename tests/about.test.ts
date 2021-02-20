f4e463eef4fed93757578db3d3c9dcd41797d0c7
scinorandex <scinorandex@gmail.com>
2021-02-20 11:44:06 +0800

    start writing tests in jest

---
+++ tests/about.test.ts
@@ -0,0 +1,8 @@
+import { Parameters } from "../src/interfaces/parameters";
+const about = require("../src/commands/about");
+const { colors } = require("../config/main.json");
+const parameters: Parameters = require("./paramters.json");
+
+test("Test the about command", () => {
+	expect(about(parameters).color).toBe(colors.green);
+});
